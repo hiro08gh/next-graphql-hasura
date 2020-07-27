de4e75cbf768a932276ce31e1df9e68aeaaed7e2
hiro08gh <hiro08gh@gmail.com>
2020-07-27 14:48:46 +0900

    add: styled-components

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,20 @@
 import { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/react-hooks';
 import { useApollo } from '../lib/apolloClient';
+import { ThemeProvider } from 'styled-components';
+import { theme } from '../styles/theme';
+
+import { GlobalStyles } from '../styles/globalStyles';
 
 export default function App({ Component, pageProps }: AppProps) {
   const apolloClient = useApollo(pageProps.initialApolloState);
 
   return (
     <ApolloProvider client={apolloClient}>
-      <Component {...pageProps} />
+      <ThemeProvider theme={theme}>
+        <GlobalStyles />
+        <Component {...pageProps} />
+      </ThemeProvider>
     </ApolloProvider>
   );
 }
